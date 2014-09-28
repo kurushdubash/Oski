2fe7824c8470826834d7edf839bea4f3f75348b8
Kurush Dubash <dubash.kurush@gmail.com>
2014-09-27 20:14:14 -0700

    FInally figured out jQuery

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 from Oski import *
 app = Flask(__name__)
 
+answer_output = ''
+
 @app.route("/")
 def hello():
 	return app.send_static_file('index.html')
@@ -11,13 +13,23 @@ def receive_cheep():
 	print(request.form)
 	return "Sucess"
 
+@app.route("/api/GetQuestion", methods=["POST"])
+def get_question():
+	transcribed_audio = request.get_json(force=True)
+	print(transcribed_audio)
+	found_answer = answer(request.get_json(force=True)['data'])
+	url_speech = text_to_voice_url(found_answer)
+	print(found_answer)
+	return render_template('answer.html', oski_answer=found_answer, speak=url_speech)
+
+	# = request.POST['data'].json()
 
 @app.route("/api/StartRecording", methods=["POST"])
 def receive_recording_cue():
 	print(request.form)
-	transcribed_audio = get_audio().capitalize()
-	print(transcribed_audio)
-	found_answer = answer(transcribed_audio)
+	# transcribed_audio = get_audio().capitalize()
+	# print(transcribed_audio)
+	found_answer = answer(answer_output)
 	url_speech = text_to_voice_url(found_answer)
 	print(found_answer)
 	return render_template('oski_answer.html', name=transcribed_audio, oski_answer=found_answer, speak=url_speech)
